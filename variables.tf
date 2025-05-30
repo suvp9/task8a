c0c02997ac08f4b74a7cb52e7213940604b1b75c
Suvham Paul <suvham_paul@epam.com>
2025-05-30 16:27:41 +0530

    fmt

--- variables.tf
+++ variables.tf
@@ -9,100 +9,100 @@ variable "acr_sku" {
   description = "sku"
 }
 variable "dockerfile_path" {
-  type = string
+  type        = string
   description = "sku"
 }
 variable "docker_build_context_path" {
-  type = string
+  type        = string
   description = "sku"
 }
 variable "docker_image_name" {
-  type = string
+  type        = string
   description = "sku"
 }
 variable "platform_os" {
-  type = string
+  type        = string
   description = "sku"
 }
 variable "acr_task_name" {
-  type = string
+  type        = string
   description = "sku"
 }
 variable "context_access_token" {
-  type = string 
+  type        = string
   description = "sku"
 }
 #aci
 variable "aci_container_environment_variables" {
-  type = map(string)
+  type        = map(string)
   description = "sku"
 }
 
 variable "aci_container_name" {
   description = "sku"
-  type = string
+  type        = string
 }
 
 variable "aci_container_cpu" {
   description = "sku"
-  type = string
+  type        = string
 }
 
 variable "aci_container_memory" {
   description = "sku"
-  type = string
+  type        = string
 }
 
 variable "aci_os_type" {
   description = "sku"
-  type = string
+  type        = string
 }
 
 
 #kv
 variable "kv_sku_name" {
   description = "sku"
-  type = string
+  type        = string
 }
 #redis
 variable "redis_primary_key_secret_name" {
   description = "sku"
-  type = string
+  type        = string
 }
 
 variable "redis_hostname_secret_name" {
-  type = string
+  type        = string
   description = "sku"
 }
 
 variable "redis_sku_name" {
-  type = string
+  type        = string
   description = "sku"
 }
 variable "redis_family" {
-  type = string
+  type        = string
   description = "sku"
 }
 variable "redis_capacity" {
-  type = number
+  type        = number
   description = "sku"
 }
 
 #aks
 variable "system_node_pool_name" {
-  type = string
+  type        = string
   description = "sku"
 }
 variable "system_node_pool_node_count" {
-  type = number
+  type        = number
   description = "sku"
 }
 variable "system_node_pool_vm_size" {
-  type = string
+  type        = string
   description = "sku"
 }
 
 variable "name_prefix" {
-  type = string
+  type        = string
   description = "sku"
 }
